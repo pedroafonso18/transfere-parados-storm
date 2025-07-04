c1cf188e40d32ff8cdfbaf41c6e5ff90bef11cd7
pedroafonso18 <pedroafonsoprogramador@gmail.com>
2025-07-04 16:18:44 -0300

    feat: Added more database functions, like insertion and fetch

--- src/database.zig
+++ src/database.zig
@@ -12,7 +12,6 @@ pub fn connect_db(db_url: []const u8, allocator: std.mem.Allocator) !*pg.Pool {
 // ============== [CONNECT DB] ============== //
 
 
-
 // ============== [FETCH DB - TYPES] ============== //
 const StormCancelados = struct {
     motivo: []const u8,
@@ -23,6 +22,12 @@ const StormCancelados = struct {
 const Vendidos = struct {
     chatid: i32,
     numero: []const u8
+};
+
+const Logs = struct {
+    chatid: []const u8,
+    corretor: []const u8,
+    sucesso: bool
 };
 // ============== [FETCH DB - TYPES] ============== //
 
@@ -53,9 +58,9 @@ pub fn fetch_vendidos(pool: *pg.Pool, allocator: std.mem.Allocator) ![]Vendidos 
     defer result.deinit();
 
     var row = try conn.query(
-    \\SELECT chat_id as chatid, phone
-    \\FROM "teste_huggy_comSaldo"
-    \\WHERE creat_time BETWEEN(NOW() - INTERVAL '1 day') AND (NOW() - INTERVAL '2 hours')
+        \\SELECT chat_id as chatid, phone
+        \\FROM "teste_huggy_comSaldo"
+        \\WHERE creat_time BETWEEN(NOW() - INTERVAL '1 day') AND (NOW() - INTERVAL '2 hours')
     );
     defer row.deinit();
 
@@ -71,4 +76,90 @@ pub fn fetch_vendidos(pool: *pg.Pool, allocator: std.mem.Allocator) ![]Vendidos 
     //TODO: Change return to verify_vendidos
     return result.toOwnedSlice();
 }
+
+pub fn fetch_cancelados(pool: *pg.Pool, allocator: std.mem.Allocator) ![]StormCancelados {
+    var conn = try pool.acquire();
+    defer conn.release();
+    var result = std.ArrayList(StormCancelados).init(allocator);
+    defer result.deinit();
+
+    var row = try conn.query(
+        \\SELECT cc.observacao_corretor, cc.telefone_celular 
+        \\FROM contratos_cancelados cc
+        \\WHERE cc.data_cancelamento::TEXT LIKE $1 || '%' 
+        \\AND cc.motivo NOT LIKE '%corretor%' 
+        \\AND cc.motivo NOT LIKE '%proposta em andamento%' 
+        \\AND cc.motivo NOT LIKE '%Solicitado%' 
+        \\AND cc.motivo NOT LIKE '%falta de assinatura%'
+        \\AND cc.sala_loja = '(PARCEIROS) MEU CONSIG - AUTOMAÇÃO'
+        \\AND NOT EXISTS (
+        \\   SELECT 1 
+        \\   FROM contratos_cancelados cc2 
+        \\   WHERE cc2.telefone_celular = cc.telefone_celular 
+        \\AND cc2.id > cc.id)
+    );
+    defer row.deinit();
+    var i: usize = 0;
+    while(row.next()) |res| :(i+=1) {
+        const obs = res.get([]const u8, 0);
+        const telefone_celular = res.get([]const u8, 1);
+        result.append(StormCancelados {
+            .chatid = "0",
+            .motivo = obs,
+            .numero = telefone_celular
+        });
+    }
+    return result.toOwnedSlice();
+}
+
+pub fn fetch_abertos(pool: *pg.Pool, allocator: std.mem.Allocator) ![][]const u8 {
+    var conn = try pool.acquire();
+    defer conn.release();
+    var result = std.ArrayList([]const u8).init(allocator);
+    defer result.deinit();
+
+    var row = try conn.query("SELECT telefone_celular FROM digitados_sistema WHERE data_insercao BETWEEN (NOW() - interval '3 days') AND (NOW() - interval '2 hours') AND status_contrato = 'aberto' AND corretor = '4716 - VENDA AUTOMATIZADA (MEU CONSIG)'", .{});
+    defer row.deinit();
+
+    var i: usize = 0;
+    while (try row.next()) |res| : (i+=1){
+        const phone = res.get([]const u8, 0);
+        try result.append(phone);
+    }
+
+    return result.toOwnedSlice();
+}
 // ============== [FETCH DB] ============== //
+
+// ============== [INSERT DB] ============== //
+pub fn log_requests(pool: *pg.Pool, logs: [][]const u8) !void {
+    var conn = try pool.acquire();
+    defer conn.release();
+
+    _ = conn.exec("INSERT INTO logs_consultados (chatid) VALUES ($1)", .{logs}) catch |err| {
+        if (conn.err) |pg_err| {
+            std.debug.print("ERROR: insert into logs_consultados failed: {s}", pg_err.message);
+        }
+        return err;
+    };
+}
+
+pub fn log_transfers(pool: *pg.Pool, logs: []Logs) !void {
+    if (logs.len == 0) {
+        std.debug.print("WARNING: logs is empty, returning...");
+        return;
+    }
+    var conn = try pool.acquire();
+    defer conn.release();
+
+
+    for (logs) |log| {
+        _ = conn.exec("INSERT INTO logs_consultados (chatid, corretor_transferido, sucesso, transfer_timestamp) VALUES ($1, $2, $3, NOW())", .{log.chatid, log.corretor, log.sucesso}) catch |err| {
+            if (conn.err) |pg_err| {
+                std.debug.print("ERROR: insert into logs_consultados for transfer failed: {s}",pg_err.message);
+            }
+            return err;
+        };
+    }
+    return;
+}
